4c694a02af6b22db922493b0c18e356c2d1f64ac
Kiran Jagathlal <kiranjagz@gmail.com>
2021-04-05 17:33:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Self.Signed.Certs.X509
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Cert!");
+        }
+    }
+}
